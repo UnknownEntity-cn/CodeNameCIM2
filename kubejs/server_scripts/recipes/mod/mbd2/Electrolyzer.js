6706c23c9818e01d0648c0102b784474a9fad62f
Qi-Month <dy757410@163.com>
2026-07-19 22:15:50 +0800

    电解机! 芜湖! 我肘赢了

--- kubejs/server_scripts/recipes/mod/mbd2/Electrolyzer.js
+++ kubejs/server_scripts/recipes/mod/mbd2/Electrolyzer.js
@@ -2,6 +2,7 @@ ServerEvents.recipes((event) => {
 	let { cmi } = event.getRecipes()
 
 	cmi.electrolyzer()
-		.inputGases("20x mekanism:steam")
-		.outputGases("10x mekanism:steam")
+		.inputItems("#minecraft:planks")
+		.outputItems("minecraft:apple")
+		.duration(20 * 20)
 })
