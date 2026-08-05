541907d59224c56c998c17747426cfdbf4b731eb
Delta <xiaofanmcplayer@outlook.com>
2026-08-05 22:54:15 +0800

    钨

--- kubejs/server_scripts/recipes/mod/immersiveengineering/Furnace.js
+++ kubejs/server_scripts/recipes/mod/immersiveengineering/Furnace.js
@@ -5,4 +5,9 @@ ServerEvents.recipes((event) => {
 	immersiveengineering.blast_furnace("4x cmi:furnaced_lime")
 		.input("create:limestone")
 		.time(30)
+	
+	// 钨锭
+	immersiveengineering.blast_furnace("cmi:tungsten_ingot")
+		.input("cmi:tungsten_mixture")
+		.time(30)
 })
