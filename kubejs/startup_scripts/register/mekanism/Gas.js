ae81f39a566c40652d0beaf3a892b7b68e3daf54
Delta <xiaofanmcplayer@outlook.com>
2026-08-13 07:57:11 +0800

    mekanism register

--- kubejs/startup_scripts/register/mekanism/Gas.js
+++ kubejs/startup_scripts/register/mekanism/Gas.js
@@ -10,19 +10,55 @@ StartupEvents.registry("mekanism:gas", (event) => {
 			.color(color)
 	}
 
+	// 硝化甘油
 	addMekanismGas("nitroglycerine", 0xFFFBD3)
+
+	// 氡
 	addMekanismGas("radon", 0x00FF00)
+
+	// 氡混合物
 	addMekanismGas("radon_mixture", 0x008F00)
+	
+	// 精炼核废料
 	addMekanismGas("refined_nuke_waste", 0x7CFC00)
+
+	// 汞
 	addMekanismGas("mercury", 0xA9C0FF)
+
+	// 汞混合物
 	addMekanismGas("mercury_mixture", 0xD2A9FF)
+
+	// 过热汞混合物
 	addMekanismGas("overheated_mercury_mixture", 0xD2A9FF)
+
+	// 氦-3
 	addMekanismGas("helium_3", 0xFFB0BA)
+
+	// 硅基气相调节器
 	addMekanismGas("silicon_gas_modulator", 0x002C55)
+
+	// 裂变铀复合物
 	addMekanismGas("fissile_uranium_compound", 0x008000) 
+
+	// 高能裂变燃料
 	addMekanismGas("high_energy_fission_fuel", 0x00294E)
+
+	// 中和钕泥浆
 	addMekanismGas("neutralized_neodymium_slurry", 0x6549B0)
+
+	// 钛氧化物
 	addMekanismGas("titanium_oxide", 0x60459E)
+
+	// 放射性混合物
 	addMekanismGas("radioactive_mixture", 0x0D3A59)
+
+	// 钚混合物
 	addMekanismGas("plutonium_mixture", 0x50A6AE)
+
+	// 核废料
+	addMekanismGas("nuke_waste", 0x4F412A)
+
+	// 用尽的核废料
+	addMekanismGas("spent_nuke_waste", 0x262015)
+
 })
