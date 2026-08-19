979aeeaa8c9859d15a89da619e17bb603dd84a4b
Qi-Month <dy757410@163.com>
2026-08-19 21:26:13 +0800

    适配KubeJS新写法

--- kubejs/server_scripts/recipes/mod/create/RnsMining.js
+++ kubejs/server_scripts/recipes/mod/create/RnsMining.js
@@ -1,32 +1,36 @@
 ServerEvents.recipes((event) => {
-	let {create_rns} = event.getRecipes()
+	let { create_rns } = event.getRecipes()
+
 	/**
 	 * 构造函数的builder
 	 * 
 	 * @param {InputItem_} depositBlock 被矿机开采的方块ID
 	 */
 	function MiningRecipe(depositBlock) {
-		this.recipe = {
-			"type": "create_rns:mining",
-			"deposit_block": depositBlock.toString(),
-			"replace_when_depleted": "create_rns:depleted_deposit_block",
-			"durability": {
-				"core": 200000,
-				"edge": 75000,
-				"random_spread": 0.2
-			},
-			"yields": []
-		}
+		this.builder = create_rns.mining()
+			.block(depositBlock.toString())
+			.replaceWhenDepleted("create_rns:depleted_deposit_block")
+			// 左右脑互搏这一块
+			// .durability({
+			// 	core: 200000,
+			// 	edge: 75000,
+			// 	random_spread: 0.2
+			// })
+			.durability(200000, 75000, 0.2)
 		return this
 	}
 	/**
 	 * 设置配方运行的维度, 若为空则默认为主世界
 	 * 
-	 * @param {ResourceLocation_} dim 配方运行的维度
+	 * @param {ResourceLocation_} dimension 配方运行的维度
 	 * @returns 
 	 */
-	MiningRecipe.prototype.dimension = function (dim) {
-		this.recipe.dimension = dim
+	MiningRecipe.prototype.dimension = function (dimension) {
+		if (dimension === "minecraft:the_nether") {
+			this.builder.nether()
+		} else if (dimension === "minecraft:overworld") {
+			this.builder.overworld()
+		}
 		return this
 	}
 	/**
@@ -35,14 +39,7 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.defaultItem = function (item) {
-		let defaultItems = {
-			items: [
-				{
-					item: item
-				}
-			]
-		}
-		this.recipe.yields.push(defaultItems)
+		this.builder.yield(y => y.item(item))
 		return this
 	}
 	/**
@@ -53,19 +50,13 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.overclockItem = function (chance, item) {
-		let overclockItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"overclock"
-			]
-		}
-		item.forEach((output) => {
-			overclockItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:overclock")
 		})
-		this.recipe.yields.push(overclockItems)
 		return this
 	}
 	/**
@@ -76,21 +67,15 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.faintResonanceItem = function (chance, item) {
-		let faintResonanceItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"faint_resonance",
-				"overclock"
-			],
-			"jei_slot_color": -6910797
-		}
-		item.forEach((output) => {
-			faintResonanceItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:faint_resonance")
+			builder.catalyst("create_rns:overclock")
+			builder.jeiSlotColor("#968CB3")
 		})
-		this.recipe.yields.push(faintResonanceItems)
 		return this
 	}
 	/**
@@ -101,21 +86,15 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.resonanceItem = function (chance, item) {
-		let resonanceItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"resonance",
-				"overclock"
-			],
-			"jei_slot_color": -8031553
-		}
-		item.forEach((output) => {
-			resonanceItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:resonance")
+			builder.catalyst("create_rns:overclock")
+			builder.jeiSlotColor("#8572BF")
 		})
-		this.recipe.yields.push(resonanceItems)
 		return this
 	}
 	/**
@@ -126,20 +105,14 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.faintShatterItem = function (chance, item) {
-		let faintShatterItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"faint_shattering_resonance"
-			],
-			"jei_slot_color": -5075058
-		}
-		item.forEach((output) => {
-			faintShatterItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:faint_shattering_resonance")
+			builder.jeiSlotColor("#B28F8E")
 		})
-		this.recipe.yields.push(faintShatterItems)
 		return this
 	}
 	/**
@@ -150,21 +123,15 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.shatterItem = function (chance, item) {
-		let shatterItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"shattering_resonance",
-				"overclock"
-			],
-			"jei_slot_color": -4229518
-		}
-		item.forEach((output) => {
-			shatterItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:shattering_resonance")
+			builder.catalyst("create_rns:overclock")
+			builder.jeiSlotColor("#BF7672")
 		})
-		this.recipe.yields.push(shatterItems)
 		return this
 	}
 
@@ -176,21 +143,15 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.faintStabilizeItem = function (chance, item) {
-		let faintStabilizeItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"faint_stabilizing_resonance",
-				"overclock"
-			],
-			"jei_slot_color": -7427662
-		}
-		item.forEach((output) => {
-			faintStabilizeItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:faint_stabilizing_resonance")
+			builder.catalyst("create_rns:overclock")
+			builder.jeiSlotColor("#8EA9B2")
 		})
-		this.recipe.yields.push(faintStabilizeItems)
 		return this
 	}
 	/**
@@ -201,36 +162,28 @@ ServerEvents.recipes((event) => {
 	 * @returns 
 	 */
 	MiningRecipe.prototype.stabilizeItem = function (chance, item) {
-		let stabilizeItems = {
-			"chance": chance,
-			"items": [],
-			"catalysts": [
-				"shattering_resonance",
-				"overclock"
-			],
-			"jei_slot_color": -9261889
-		}
-		item.forEach((output) => {
-			stabilizeItems.items.push({
-				"item": output
+		this.builder.yield((builder) => {
+			builder.chance(chance)
+			item.forEach((output) => {
+				builder.item(output)
 			})
+			builder.catalyst("create_rns:stabilizing_resonance")
+			builder.catalyst("create_rns:overclock")
+			builder.jeiSlotColor("#72ACBF")
 		})
-		this.recipe.yields.push(stabilizeItems)
 		return this
 	}
 	/**
 	 * 最终执行配方的构建, 是实际输出配方的链式方法
 	 * 该链式方法需要置于最后
 	 * 
-	 * @param {string} id 配方ID, 为空则自动生成kjs id
+	 * @param {string} [id] 配方ID, 为空则自动生成kjs id
 	 */
 	MiningRecipe.prototype.build = function (id) {
-		let builder = event.custom(this.recipe)
 		if (id != null) {
-			builder.id(id)
+			this.builder.id(id)
 		}
 	}
-
 	new MiningRecipe("cmi:coal_deposit_block")
 		.defaultItem(["cmi:deposit_dust"])
 		.overclockItem(5.0E-4, ["cmi:deposit_dust", "mekanism:dust_coal"])
