33101671287ac71458deda25f02f1d7ba8b496c0
Qi-Month <dy757410@163.com>
2026-08-26 01:04:26 +0800

    1

--- kubejs/startup_scripts/register/item/equipment/Armors.js
+++ kubejs/startup_scripts/register/item/equipment/Armors.js
@@ -1,5 +1,5 @@
 StartupEvents.registry("item", (event) => {
 	event.create(`${Cmi.MODID}:bucket`, "helmet")
-		.texture(Cmi.loadResource("item/armor/bucket"))
 		.tier(`${Cmi.MODID}:iron`)
+		.texture(Cmi.loadResource("item/armor/bucket"))
 })
