5893cc57376732c463c87f54d71297cc5f92f455
逐日炎雪中微子 <eternalsnowstorm@foxmail.com>
2026-08-28 01:36:40 +0800

    成功修复JsonIO

--- kubejs/server_scripts/data/unification/MetalMaterials.js
+++ kubejs/server_scripts/data/unification/MetalMaterials.js
@@ -1,60 +1,65 @@
 // priority: 9
-let materialTypes = [
-	"plate",
-	"nugget",
-	"storage_block",
-	"gear",
-	"dust",
-	"rod",
-	"wire"
-]
-CmiMetalRegistry.getAll().forEach((metal) => {
-	let material = String(metal.getId())
-	let ingotTag = `#forge:ingots/${material}`
-	let ingotResult = getHighPriorityItem(ingotTag)
-
-	let unification = [
-		{
-			matchItems: [ingotTag],
-			resultItems: ingotResult
-		}
+ServerEvents.afterRecipes((event) => {
+	let materialTypes = [
+		"plate",
+		"nugget",
+		"storage_block",
+		"gear",
+		"dust",
+		"rod",
+		"wire"
 	]
+	CmiMetalRegistry.getAll().forEach((metal) => {
+		let material = String(metal.getId())
+		let ingotTag = `#forge:ingots/${material}`
+		let ingotResult = getHighPriorityItem(ingotTag)
 
+		let unification = [
+			{
+				matchItems: [ingotTag],
+				resultItems: ingotResult
+			}
+		]
 
-	materialTypes.forEach((type) => {
-		let tag = `#forge:${type}s/${material}`
-		let result = getHighPriorityItem(tag)
+		materialTypes.forEach((type) => {
+			let tag = `#forge:${type}s/${material}`
+			let result = getHighPriorityItem(tag)
+
+			unification.push({
+				matchItems: [tag],
+				resultItems: result
+			})
+		})
+
+		let rawTag = `#forge:raw_materials/${material}`
+		let rawResult = getHighPriorityItem(rawTag)
 
 		unification.push({
-			matchItems: [tag],
-			resultItems: result
-		})
-	})
-
-	let rawTag = `#forge:raw_materials/${material}`
-	let rawResult = getHighPriorityItem(rawTag)
-
-	unification.push({
-		matchItems: [rawTag],
-		resultItems: rawResult
-	})
-
-
-	let blockTag = `#forge:storage_blocks/raw_${material}`
-	let blockResult = getHighPriorityItem(blockTag)
-
-	unification.push({
-		matchItems: [blockTag],
-		resultItems: blockResult
-	})
-
-	let validUnification = unification
-		.filter((entry) => {
-			entry.resultItems != null && entry.resultItems !== "cmi:cmi_icon"
+			matchItems: [rawTag],
+			resultItems: rawResult
 		})
 
-	console.log(validUnification)
-	JsonIO.writeAndCreateDirectories(`kubejs/data/oei/replacements/${material}.json`, validUnification)
+		let blockTag = `#forge:storage_blocks/raw_${material}`
+		let blockResult = getHighPriorityItem(blockTag)
+
+		unification.push({
+			matchItems: [blockTag],
+			resultItems: blockResult
+		})
+
+		let validUnification = unification.filter((entry) => {
+			return entry.resultItems != null && entry.resultItems !== "cmi:cmi_icon"
+		})
+
+		let $Files = Java.loadClass("java.nio.file.Files")
+		let $Paths = Java.loadClass("java.nio.file.Paths")
+		let filePath = $Paths.get(`kubejs/data/oei/replacements/${material}.json`)
+
+		$Files.createDirectories(filePath.getParent())
+		$Files.writeString(filePath, JsonIO.toPrettyString(JsonIO.of(validUnification)))
+
+		console.log(`[OEI] Wrote ${filePath} with ${validUnification.length} replacement(s)`)
+	})
 })
 
 /**
