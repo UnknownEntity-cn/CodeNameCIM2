9c20a848f3b120ddf752f9f51af7d806087b2a4f
Delta <xiaofanmcplayer@outlook.com>
2026-08-30 21:14:30 +0800

    给谢尔盖的配方

--- kubejs/server_scripts/recipes/mod/create/ItemApplication.js
+++ kubejs/server_scripts/recipes/mod/create/ItemApplication.js
@@ -6,4 +6,11 @@ ServerEvents.recipes((event) => {
 		"minecraft:obsidian",
 		"#forge:vegetables/onion"
 	])
+
+	// 可疑的龙蛋
+	create.item_application("kaleidoscope_end:suspicious_dragon_egg", [
+		"minecraft:dragon_egg",
+		"#forge:dusts/chaotic_void"
+	])
+
 })
