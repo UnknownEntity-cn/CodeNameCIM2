b9ef8e2d397e645add9d5222d3f33cbff5c3aea5
Qi-Month <dy757410@163.com>
2026-09-01 00:38:08 +0800

    学术造假重做

---
+++ kubejs/server_scripts/event/advancements/AcademicFraud.js
@@ -0,0 +1,39 @@
+let $AdvancementEvent$AdvancementEarnEvent =
+	Java.loadClass("net.minecraftforge.event.entity.player.AdvancementEvent$AdvancementEarnEvent")
+
+nativeEvent($AdvancementEvent$AdvancementEarnEvent, (event) => {
+	let player = event.getEntity()
+	let advancement = event.getAdvancement()
+
+	if (player instanceof Player) {
+		if (advancement.getId() !== "cmi:academic_fraud") {
+			return
+		}
+
+		let count = getIronNeeded(player)
+		player.give(Item.of("minecraft:iron_ingot", count))
+	}
+})
+
+/**
+ * 计算填满玩家背包所需的铁锭数量
+ *
+ * @param {Player_} player 
+ * @returns {number} 填满背包所需的铁锭数量
+ */
+function getIronNeeded(player) {
+	let inventory = player.getInventory()
+	let needed = 0
+
+	for (let i = 0; i < 36; i++) {
+		let stack = inventory.getItem(i)
+
+		if (stack.isEmpty()) {
+			needed += 64
+		} else if (stack.getId() === "minecraft:iron_ingot") {
+			needed += 64 - stack.getCount()
+		}
+	}
+
+	return needed
+}
